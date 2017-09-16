bbd2be25580f701355f0d186739cbce27decb652
Stephanie Van laere <stephanievanlaere@v1021-wn-63-220.campus-dynamic.uwaterloo.ca>
2017-09-16 09:12:22 +0200

    test

--- backend/routegen/route.py
+++ backend/routegen/route.py
@@ -1,6 +1,10 @@
 import geog
 import networkx as nx
 import osmgraph
+import random
+import itertools
+import geojsonio
+import json
 
 # By default any way with a highway tag will be loaded
 g = osmgraph.parse_file('boston_massachusetts.osm.bz2')  # or .osm or .pbf
@@ -8,8 +12,6 @@ for n1, n2 in g.edges_iter():
     c1, c2 = osmgraph.tools.coordinates(g, (n1, n2))   
     g[n1][n2]['length'] = geog.distance(c1, c2)
 
-
-import random
 start = random.choice(g.nodes())
 end = random.choice(g.nodes())
 path = nx.shortest_path(g, start, end, 'length')
@@ -17,19 +19,16 @@ coords = osmgraph.tools.coordinates(g, path)
 
 # Find the sequence of roads to get from start to end
 edge_names = [g[n1][n2].get('name') for n1, n2 in osmgraph.tools.pairwise(path)]
-import itertools
 names = [k for k, v in itertools.groupby(edge_names)]
 print(names)
-     ['North Harvard Street',
-      'Franklin Street',
-      'Lincoln Street',
-      None,
-      'Cambridge Street',
-      'Gordon Street',
-      'Warren Street',
-      'Commonwealth Avenue']
+    #  ['North Harvard Street',
+    #   'Franklin Street',
+    #   'Lincoln Street',
+    #   None,
+    #   'Cambridge Street',
+    #   'Gordon Street',
+    #   'Warren Street',
+    #   'Commonwealth Avenue']
 
 # Visualize the path using geojsonio.py
-import geojsonio
-import json
 geojsonio.display(json.dumps({'type': 'LineString', 'coordinates': coords}))
